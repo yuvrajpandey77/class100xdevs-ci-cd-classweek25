6a044728df5db21241051e22417a654aade0c1cc
Yuvraj Pandey <email@gmail.com>
2025-06-13 20:39:41 +0000

    deploy script  5

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
             cd class100xdevs-ci-cd-classweek25
             git pull origin main
             npm install
-            npm run build
+            # NEXT_TELEMETRY_DISABLED=1 npm run build --no-lint
             npm run start

af4f794097d15229a0896eff3abaa68321778d59
Yuvraj Pandey <email@gmail.com>
2025-06-13 21:11:25 +0000

    deploy script  12

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# Clone repository if it doesn't exist
-if [ ! -d "class100xdevs-ci-cd-classweek25" ]; then
-    git clone https://github.com/yuvrajpandey77/class100xdevs-ci-cd-classweek25.git
-fi
-
-cd class100xdevs-ci-cd-classweek25
-git pull origin main
-npm install
-npm run build
-npm run start
+# This file is no longer used. Deployment is handled by GitHub Actions workflow.

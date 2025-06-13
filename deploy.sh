3c52eb2f5d8d61a682eb1b8580d484c600e8ce70
Yuvraj Pandey <email@gmail.com>
2025-06-13 20:44:49 +0000

    deploy script  7

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
-            cd class100xdevs-ci-cd-classweek25
-            git pull origin main
-            npm install
-            NEXT_TELEMETRY_DISABLED=1 npm run build --no-lint
-            npm run start
+#!/bin/bash
+
+# Clone repository if it doesn't exist
+if [ ! -d "class100xdevs-ci-cd-classweek25" ]; then
+    git clone https://github.com/yuvrajpandey77/class100xdevs-ci-cd-classweek25.git
+fi
+
+cd class100xdevs-ci-cd-classweek25
+git reset --hard
+git clean -fd
+git pull origin main
+npm install
+NEXT_TELEMETRY_DISABLED=1 npm run build --no-lint
+npm run start
